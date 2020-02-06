ae1272b89584f3859b7a18021ea7fcaaa97ededc
Henry Bi <henrybi@ripl.com>
2020-02-06 10:40:28 -0800

    Added input component for guessing letter.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default class App extends React.Component
     super();
     this.state = {
       guessWordArr: randomWords().split( "" ),
+      guessedLetter: "",
     }
   }
 
@@ -21,9 +22,19 @@ export default class App extends React.Component
         <div className="container">
           <h1>React Hangman</h1>
           <h2>Word to Quess: {this.state.guessWordArr}</h2>
+          <h3>Letter You have submited: {this.state.guessedLetter}</h3>
+          <form onSubmit={this.handleGuessSubmitted}>
+            <label>Guess a letter:
+              <input type="text" name="name" minLength="1" maxLength="1" required onChange={this.handleChange}/>
+            </label>
+            <input type="submit" value="Submit"/>
+          </form>
           <Hangman incorrectGuessCount={10}></Hangman>
         </div>
       </div>
     );
   }
+
+  handleGuessSubmitted = event => this.setState( {guessedLetterSubmitted: event.target.value} );
+  handleChange = event => this.setState( {guessedLetter: event.target.value} );
 };

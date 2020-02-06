0429014cdbf28eb7cc959a9d0f39ed79b067062f
Henry Bi <henrybi@ripl.com>
2020-02-06 10:50:25 -0800

    Implemented guess history, added logic for not resubmit letters that user guessed.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default class App extends React.Component
     this.state = {
       guessWordArr: randomWords().split( "" ),
       guessedLetter: "",
+      guessedLetters: [],
     }
   }
 
@@ -22,10 +23,11 @@ export default class App extends React.Component
         <div className="container">
           <h1>React Hangman</h1>
           <h2>Word to Quess: {this.state.guessWordArr}</h2>
+          <h3>You have already guessed these letters: {this.state.guessedLetters}</h3>
           <h3>Letter You have submited: {this.state.guessedLetter}</h3>
           <form onSubmit={this.handleGuessSubmitted}>
             <label>Guess a letter:
-              <input type="text" name="name" minLength="1" maxLength="1" required onChange={this.handleChange}/>
+              <input required type="text" name="name" minLength="1" maxLength="1" value={this.state.guessedLetter} onChange={this.handleChange}/>
             </label>
             <input type="submit" value="Submit"/>
           </form>
@@ -35,6 +37,18 @@ export default class App extends React.Component
     );
   }
 
-  handleGuessSubmitted = event => this.setState( {guessedLetterSubmitted: event.target.value} );
   handleChange = event => this.setState( {guessedLetter: event.target.value} );
+  handleGuessSubmitted = ( event ) =>
+  {
+    event.preventDefault();
+    if ( this.state.guessedLetters.includes( this.state.guessedLetter ) )
+    {
+      window.alert( `you have already guessed ${this.state.guessedLetter}, please try a different letter` );
+      this.setState( {guessedLetter: ""} );
+    }
+    else
+    {
+      this.setState( {guessedLetters: this.state.guessedLetters.concat( this.state.guessedLetter )} )
+    }
+  };
 };

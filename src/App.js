bf1ce4f5fa6d4b9ba056dc7a5652e9184fc406d6
Henry Bi <henrybi@ripl.com>
2020-02-06 10:03:58 -0800

    Installed random word package and displayed on page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Hangman } from './components';
+import {Hangman} from './components';
+import * as randomWords from 'random-words';
 
 import './App.css';
 
-const App = () => {
+const App = () =>
+{
   return (
     <div className="App">
       <div className="container">
         <h1>React Hangman</h1>
+        <h2>Word to Quess: {randomWords()}</h2>
         <Hangman incorrectGuessCount={10}></Hangman>
       </div>
     </div>

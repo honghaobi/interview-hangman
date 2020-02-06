9300f1308f254505193f44d9958842629edf949e
Henry Bi <henrybi@ripl.com>
2020-02-06 15:24:11 -0800

    Remove unused game status.

--- src/components/Hangman/GameStatus.jsx
+++ src/components/Hangman/GameStatus.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-export const GameStatus = ( {incorrectLetters, correctLetters, incorrectGuessCount, gameStatus} ) =>
+export const GameStatus = ( {incorrectLetters, correctLetters, incorrectGuessCount} ) =>
 {
   return (
     <div>
       <h3>Incorrect Letters: {incorrectLetters}</h3>
       <h3>Correct Letters: {correctLetters}</h3>
       <h3>Number of Guesses Left: {10 - incorrectGuessCount}</h3>
-      <h3>{gameStatus}</h3>
     </div>
   );
 };

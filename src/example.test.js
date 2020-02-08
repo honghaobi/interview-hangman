ed85e34b72ebc9af2acee27f68a9fa92fc9d16b1
Henry Bi <henrybi@ripl.com>
2020-02-07 17:33:27 -0800

    Added first test suites for component renders without errors.

--- src/example.test.js
+++ src/example.test.js
@@ -1,17 +1,47 @@
-import * as randomWords from 'random-words';
+import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
+import {GameStatus, GuessDisplay, GuessLetterSubmit, Hangman} from "./components/Hangman";
 
-describe( 'Example Test', () =>
+describe( 'All Components can render without error', function ()
 {
-  it( 'is just an example showing jest is working', () =>
+
+  it( 'can render GameStatus Component without error', function ()
   {
-    expect( true ).toBe( true );
+    let component, element;
+    element = React.createElement( GameStatus );
+    expect( () =>
+    {
+      component = ReactTestUtils.renderIntoDocument( element );
+    } ).not.toThrow();
   } );
-} );
-describe( 'Random word generator tests', () =>
-{
-  it( 'it generates a random word', () =>
+
+  it( 'can render GuessLetterSubmit Component without error', function ()
   {
-    const guessWord = randomWords();
-    expect( guessWord ).toBe( true );
+    let component, element;
+    element = React.createElement( GuessLetterSubmit );
+    expect( () =>
+    {
+      component = ReactTestUtils.renderIntoDocument( element );
+    } ).not.toThrow();
   } );
-} );
+
+  it( 'can render GuessDisplay Component without error', function ()
+  {
+    let component, element;
+    element = React.createElement( GuessDisplay );
+    expect( () =>
+    {
+      component = ReactTestUtils.renderIntoDocument( element );
+    } ).not.toThrow();
+  } );
+
+  it( 'can render Hangman Component without error', function ()
+  {
+    let component, element;
+    element = React.createElement( Hangman, {incorrectGuessCount: 10} );
+    expect( () =>
+    {
+      component = ReactTestUtils.renderIntoDocument( element );
+    } ).not.toThrow();
+  } );
+} )

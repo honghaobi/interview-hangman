a22d77871629f268583eaf4fa14df2a55d614e94
Henry Bi <henrybi@ripl.com>
2020-02-09 18:39:02 -0800

    Updated code style formatting.

--- src/hangmanGame.test.js
+++ src/hangmanGame.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactTestUtils from 'react-dom/test-utils';
 import ShallowRenderer from 'react-test-renderer/shallow';
-import {GameStatus, GuessDisplay, GuessLetterSubmit, Hangman} from "./components/Hangman";
+import {GameStatus, GuessDisplay, GuessLetterSubmit, Hangman} from './components/Hangman';
 
 let mockInitialState = {
-  word: "hello",
-  guessingLetter: "",
-  incorrectLetters: "",
-  incorrectGuessCount: 0,
-  correctLetters: "",
+  word: 'hello',
+  guessingLetter: '',
+  incorrectLetters: '',
+  correctLetters: '',
 };
 
 let state, component, element, renderer;
@@ -32,7 +31,6 @@ describe( 'All Components can render without error', function ()
       {
         incorrectLetters: state.incorrectLetters,
         correctLetters: state.correctLetters,
-        incorrectGuessCount: state.incorrectGuessCount
       } );
     expect( () =>
     {
@@ -72,7 +70,7 @@ describe( 'All Components can render without error', function ()
 
   it( 'can render Hangman Component without error', function ()
   {
-    element = React.createElement( Hangman, {incorrectGuessCount: state.incorrectGuessCount} );
+    element = React.createElement( Hangman, {incorrectGuessCount: 1} );
     expect( () =>
     {
       component = ReactTestUtils.renderIntoDocument( element );
@@ -85,7 +83,6 @@ describe( 'GuessDisplay Component rendering', function ()
   beforeAll( () =>
   {
     state = mockInitialState;
-    renderer = new ShallowRenderer();
   } );
 
   beforeEach( () =>
@@ -100,7 +97,7 @@ describe( 'GuessDisplay Component rendering', function ()
 
   it( 'render for no correct letters guessed', function ()
   {
-    renderer.render( <GuessDisplay word={state.word} correctLetters=""/> );
+    renderer.render( <GuessDisplay word={state.word} correctLetters=''/> );
     const result = renderer.getRenderOutput();
     expect( result.type ).toBe( 'span' );
     expect( result.props.children ).toEqual(
@@ -110,7 +107,7 @@ describe( 'GuessDisplay Component rendering', function ()
 
   it( 'render for 1 correct letters guessed', function ()
   {
-    renderer.render( <GuessDisplay word={state.word} correctLetters="e"/> );
+    renderer.render( <GuessDisplay word={state.word} correctLetters='e'/> );
     const result = renderer.getRenderOutput();
     expect( result.props.children ).toEqual(
       ['_', 'e', '_', '_', '_']
@@ -119,7 +116,7 @@ describe( 'GuessDisplay Component rendering', function ()
 
   it( 'render for 1 correct letters guessed for two places in the word', function ()
   {
-    renderer.render( <GuessDisplay word={state.word} correctLetters="l"/> );
+    renderer.render( <GuessDisplay word={state.word} correctLetters='l'/> );
     const result = renderer.getRenderOutput();
     expect( result.props.children ).toEqual(
       ['_', '_', 'l', 'l', '_']
@@ -141,7 +138,6 @@ describe( 'GameStatus Component rendering', function ()
   beforeAll( () =>
   {
     state = mockInitialState;
-    renderer = new ShallowRenderer();
   } );
 
   beforeEach( () =>
@@ -156,52 +152,52 @@ describe( 'GameStatus Component rendering', function ()
 
   it( 'render for 1 correct letter and 1 incorrect letter', function ()
   {
-    renderer.render( <GameStatus incorrectLetters="x" correctLetters="h"/> );
+    renderer.render( <GameStatus incorrectLetters='x' correctLetters='h'/> );
     const result = renderer.getRenderOutput();
     const parentDiv = result.props.children;
     expect( result.type ).toBe( 'div' );
     expect( parentDiv[0].props.children ).toEqual(
-      ["Incorrect Letters: ", "x"]
+      ['Incorrect Letters: ', 'x']
     );
     expect( parentDiv[1].props.children ).toEqual(
-      ["Correct Letters: ", "h"]
+      ['Correct Letters: ', 'h']
     );
     expect( parentDiv[2].props.children ).toEqual(
-      ["Number of Guesses Left: ", 9]
+      ['Number of Guesses Left: ', 9]
     );
   } );
 
   it( 'render for 4 correct letter and 0 incorrect letter', function ()
   {
-    renderer.render( <GameStatus incorrectLetters="" correctLetters="helo"/> );
+    renderer.render( <GameStatus incorrectLetters='' correctLetters='helo'/> );
     const result = renderer.getRenderOutput();
     const parentDiv = result.props.children;
     expect( result.type ).toBe( 'div' );
     expect( parentDiv[0].props.children ).toEqual(
-      ["Incorrect Letters: ", ""]
+      ['Incorrect Letters: ', '']
     );
     expect( parentDiv[1].props.children ).toEqual(
-      ["Correct Letters: ", "helo"]
+      ['Correct Letters: ', 'helo']
     );
     expect( parentDiv[2].props.children ).toEqual(
-      ["Number of Guesses Left: ", 10]
+      ['Number of Guesses Left: ', 10]
     );
   } );
 
   it( 'render for 0 correct letter and 10 incorrect letter', function ()
   {
-    renderer.render( <GameStatus incorrectLetters="abcdefghij" correctLetters=""/> );
+    renderer.render( <GameStatus incorrectLetters='abcdefghij' correctLetters=''/> );
     const result = renderer.getRenderOutput();
     const parentDiv = result.props.children;
     expect( result.type ).toBe( 'div' );
     expect( parentDiv[0].props.children ).toEqual(
-      ["Incorrect Letters: ", "abcdefghij"]
+      ['Incorrect Letters: ', 'abcdefghij']
     );
     expect( parentDiv[1].props.children ).toEqual(
-      ["Correct Letters: ", ""]
+      ['Correct Letters: ', '']
     );
     expect( parentDiv[2].props.children ).toEqual(
-      ["Number of Guesses Left: ", 0]
+      ['Number of Guesses Left: ', 0]
     );
   } );
 } );
